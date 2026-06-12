f304f2fa412881abf94e95fc615db449518441b9
curt-888 <curt19888@gmail.com>
2026-06-12 12:13:53 +0800

    更新SW缓存版本强制刷新手机端

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='english-little-paradise-v1';
+const CACHE='english-little-paradise-v2';
 const ASSETS=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',e=>{
